d4977879502eba32e5118564a8068fa4f67c7715
Damien Rodriguez <damienr2017@gmail.com>
2026-03-10 11:11:29 -0700

    updated logout to work correctly

--- selenium-training-app/src/app/core/layout/layout.ts
+++ selenium-training-app/src/app/core/layout/layout.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
+import { AuthService } from '../services/auth';
 
 @Component({
   selector: 'app-layout',
@@ -19,4 +20,12 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
-export class Layout {}
+export class Layout {
+  private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
