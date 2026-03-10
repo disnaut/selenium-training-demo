955fbb8f162395f4229fa657db4596af34c80647
Damien Rodriguez <damienr2017@gmail.com>
2026-03-10 11:09:22 -0700

    added logout button to layout mat-toolbar

--- selenium-training-app/src/app/core/layout/layout.ts
+++ selenium-training-app/src/app/core/layout/layout.ts
@@ -3,6 +3,7 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-layout',
@@ -13,6 +14,7 @@ import { MatListModule } from '@angular/material/list';
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
+    MatButtonModule,
   ],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
